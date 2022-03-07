4b8440be7e7801c0ff81573b02d4406e59cd0bfd
sophiefitone <sophiefitone231@gmail.com>
2022-03-07 09:46:59 +0100

    change5

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
  return (
 
-    <div className='footer'> 
+    <div className=' containfooter'> 
       <h2> &copy; Copyright.</h2>
       <p>Made by Sophie Fitone</p>
 
